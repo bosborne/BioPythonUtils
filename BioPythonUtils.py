be92382cfad2d411312354214a53c4b755166a7e
Brian Osborne <briano@bioteam.net>
2014-10-01 22:24:23 -0400

    Simpler logic

--- BioPythonUtils.py
+++ BioPythonUtils.py
@@ -168,6 +168,7 @@ class TranslateCommand(sublime_plugin.TextCommand):
                 self.view.window().new_file().insert(
                     edit, 0, seqout.getvalue())
 
+            # If selection is not Fasta
             else:
                 seqout = []
                 # Could be more than one sequence, "MULTILINE" required 
@@ -212,30 +213,31 @@ class GenbankToFastaCommand(sublime_plugin.TextCommand):
             seq_str = self.view.substr(region)
             seq_str = seq_str.strip()
 
-            if seq_str:
-                # Check that the selection begins as expected
-                startmatch = re.match(r'^LOCUS', seq_str)
-                # It turns out that SeqIO can handle Genbank format that
-                # does not end in '//' so there is no need to check for this
-
-                if not startmatch:
-                    sublime.error_message(
-                        "Selected text does not look like Genbank: no 'LOCUS'")
-                    return
-                else:
-                    # Read from a string and write to a string
-                    seqout = io.StringIO()
-
-                    with io.StringIO(seq_str) as seqin:
-                        SeqIO.convert(seqin, 'genbank', seqout, 'fasta')
-                    seqin.close()
-
-                    # Write the fasta string to a new window at position 0
-                    self.view.window().new_file().insert(
-                        edit, 0, seqout.getvalue())
-            else:
+            if not seq_str:
                 sublime.error_message("No selected text")
                 return
+
+            # Check that the selection begins as expected
+            startmatch = re.match(r'^LOCUS', seq_str)
+            # It turns out that SeqIO can handle Genbank format that
+            # does not end in '//' so there is no need to check for this
+
+            if startmatch:
+                # Read from a string and write to a string
+                seqout = io.StringIO()
+
+                with io.StringIO(seq_str) as seqin:
+                        SeqIO.convert(seqin, 'genbank', seqout, 'fasta')
+                seqin.close()
+
+                # Write the fasta string to a new window at position 0
+                self.view.window().new_file().insert(
+                        edit, 0, seqout.getvalue())
+            else:
+                sublime.error_message(
+                "Selected text does not look like Genbank: no 'LOCUS'")
+                return
+            
 
 # "Remote Blast"
 class RemoteBlastCommand(sublime_plugin.TextCommand):
