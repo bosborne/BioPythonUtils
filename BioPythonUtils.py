0f539ccf244090d36e103025c108d9919f78e043
Brian Osborne <briano@bioteam.net>
2016-12-19 20:57:00 -0500

    Formatting

--- BioPythonUtils.py
+++ BioPythonUtils.py
@@ -64,7 +64,7 @@ class DownloadSequenceBySearchCommand(sublime_plugin.TextCommand):
             print("Entrez count: {}".format(ids['Count']))
             dialog_result = sublime.ok_cancel_dialog(
                 "Download {0} sequences? 'retmax': {1}".format(ids['Count'],
-                entrez_retmax),'Download')
+                    entrez_retmax), 'Download')
 
             if dialog_result is True:
                 try:
